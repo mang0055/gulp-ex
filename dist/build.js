47fa3de02a297688f8c20fa98d8fc4318fb6ed9d
Raviraj Mangukiya <ravirajcm@gmail.com>
2016-10-28 12:24:30 -0400

    Controllers added in gulp

--- dist/build.js
+++ dist/build.js
@@ -1,1 +1,1 @@
-angular.module("AwesomeApp",["ui-router"]);
+angular.module("AwesomeApp",["ui-router"]);angular.module("AwesomeApp").controller("MainCtrl",["$scope",function(a){a.title="Awesome App"}]);
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var closureCompiler = require('gulp-closure-compiler');
  
 gulp.task('default', function() {
-  return gulp.src('js/app.js')
+  return gulp.src(['js/app.js','js/controllers/*js'])
     .pipe(closureCompiler({
       compilerPath: 'bower_components/closure-compiler/lib/closure-compiler-v20161024.jar',
       fileName: 'build.js'
